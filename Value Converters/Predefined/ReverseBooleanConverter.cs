fbdd2573d3ba23f66594d25ee7970f0f10fe1665
AgentFire <agentfire@mail.ru>
2017-01-01 18:22:44 +0300

    Added ReverseBoolean converter.

---
+++ Value Converters/Predefined/ReverseBooleanConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace AgentFire.Wpf.ValueConverters.Predefined
+{
+    /// <summary>
+    /// Simply negates any boolean value.
+    /// </summary>
+    public sealed class ReverseBooleanConverter : ValueConverterBase<bool, bool>
+    {
+        /// <summary>
+        /// Default value: <see cref="false"/>. Set to <see cref="true"/> to enable only source=>target reversion, but not vise versa.
+        /// </summary>
+        public bool OneWayReverse { get; set; } = false;
+
+        protected override bool Convert(bool source)
+        {
+            return !source;
+        }
+        protected override bool ConvertBack(bool source)
+        {
+            return OneWayReverse ? source : !source;
+        }
+    }
+}
